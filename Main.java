188f7c0fcb7986a6f340e45ec3d956058305cbab
NAMANDEEP SINGH <namandeepsingh208@gmail.com>
2020-04-21 09:26:53 +0530

    Added some Inputs

--- Main.java
+++ Main.java
@@ -5,10 +5,12 @@ public class Main
 	    list.addLast(10,list);
 	    list.addLast(20,list);
 	    list.addLast(30,list);
+         list.addLast(50,list);
+	    list.addFront(60,list);
 	    list.printLinkedList(list);
 	    System.out.println();
 	    System.out.println("Size of List: "+list.size);
-	    System.out.println(list.head);
+	    System.out.println(list.head.data);
 	    
 	
 		
