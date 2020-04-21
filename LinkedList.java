a229357405aee371b837ad33d18d78adfcface87
NAMANDEEP SINGH <namandeepsingh208@gmail.com>
2020-04-21 08:28:19 +0530

    Linked List class with Adding element at last

---
+++ LinkedList.java
@@ -0,0 +1,58 @@
+public class LinkedList{
+    public Node head;
+    public static int size;
+    
+    public LinkedList(){
+        head=null;
+        size=0;
+    }
+    
+    public static void addLast(int data, LinkedList list){
+        Node newNode = new Node(data);
+        newNode.nextNode=null;
+        
+        if(list.head==null){
+            list.head=newNode;
+        }
+        
+        else{
+            Node last=list.head;
+            while(last.nextNode!=null){
+                last=last.nextNode;
+            }
+            last.nextNode=newNode;
+        }
+        size++;
+     
+    }
+    
+    // public static void addFront(int data,LinkedList list){
+    //     Node newNode=new Node(data,null);
+    //     if(list.head==null){
+    //         list.head=newNode;
+    //     }
+    //     else{
+    //         list.head=newNode;
+    //         newNode.nextNode=list.head;
+    //     }
+        
+    // }
+    public static void printLinkedList(LinkedList list){
+        
+        Node curNode=list.head;
+        if(curNode==null){
+            System.out.println("List is empty!");
+        }
+        else{
+            while(curNode!=null){
+                System.out.print(curNode.data+"->");
+                curNode=curNode.nextNode;
+            }
+            if(curNode==null)
+            System.out.print("null");
+        
+        }
+        
+    }
+    
+}
